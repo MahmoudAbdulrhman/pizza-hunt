bec914f55a2e549d801f1c092f4678fd351512a0
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2020-11-18 00:11:02 -0800

    setup comments'

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -6,7 +6,18 @@ const pizzaController = {
     //get all pizzas
     getAllPizza(req, res) {
         Pizza.find({})
+            
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            
+            .select('-__v')
+           
+            .sort({ _id: -1 })
+           
             .then(dbPizzaData => res.json(dbPizzaData))
+           
             .catch(err => {
                 console.log(err);
                 res.status(400).json(err);
@@ -16,6 +27,14 @@ const pizzaController = {
     //get one pizza by id
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
+        
+        .populate({
+            path:'comments',
+            select: '-__v'
+        })
+        
+        .select('-__v')
+
         .then(dbPizzaData => {
             // If no pizza is found, send 404
             if (!dbPizzaData) {
